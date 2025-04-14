b0e34b68f772c3a49c6bffe22c4d07075d836829
Feishi Wang <feishiwang2001@gmail.com>
2025-04-14 00:59:40 -0700

    update scripts/transfer.sh to take a list of folders instead of a single folder

--- scripts/transfer.sh
+++ scripts/transfer.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-path=$1
-target_path=${path/roboverse_data_dev/roboverse_data}
+for path in "$@"; do
+    target_path=${path/roboverse_data_dev/roboverse_data}
 
-target_dir=$(dirname "$target_path")
-if [ ! -d "$target_dir" ]; then
-    mkdir -p "$target_dir"
-fi
+    target_dir=$(dirname "$target_path")
+    if [ ! -d "$target_dir" ]; then
+        mkdir -p "$target_dir"
+    fi
 
-cp -r "$path" "$target_path"
+    cp -r "$path" "$target_path"
+done
