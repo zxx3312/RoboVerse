6ff093bc051fbc2ba188d0bd9de4c257759f4a55
Feishi Wang <feishiwang2001@gmail.com>
2025-04-21 23:16:57 -0700

    print_usd_joint_names also print body names

--- scripts/print_usd_joint_names.py
+++ scripts/print_usd_joint_names.py
@@ -34,3 +34,7 @@ joint_limits = obj.root_physx_view.get_dof_limits().squeeze(0).tolist()
 # print(obj.root_physx_view.get_dof_position_targets())
 for joint_name, joint_limit in zip(obj.joint_names, joint_limits):
     print(f"{joint_name}: ({joint_limit[0]:.4f}, {joint_limit[1]:.4f})")
+
+print("=" * 100)
+print(obj.body_names)
+print("=" * 100)
