f04cbcbb04e5dce6e3e9dc57ad8d61b31f46a78c
Feishi Wang <feishiwang2001@gmail.com>
2025-04-28 22:15:16 -0700

    add get_started example for multiple cameras

---
+++ get_started/multiple_cameras.py
@@ -0,0 +1,153 @@
+"""This script is used to test multiple cameras."""
+
+from __future__ import annotations
+
+from typing import Literal
+
+try:
+    import isaacgym  # noqa: F401
+except ImportError:
+    pass
+
+import os
+
+import imageio
+import numpy as np
+import rootutils
+import torch
+import tyro
+from loguru import logger as log
+from rich.logging import RichHandler
+
+rootutils.setup_root(__file__, pythonpath=True)
+log.configure(handlers=[{"sink": RichHandler(), "format": "{message}"}])
+
+
+from metasim.cfg.objects import ArticulationObjCfg, PrimitiveCubeCfg, PrimitiveSphereCfg, RigidObjCfg
+from metasim.cfg.scenario import ScenarioCfg
+from metasim.cfg.sensors import PinholeCameraCfg
+from metasim.constants import PhysicStateType, SimType
+from metasim.utils import configclass
+from metasim.utils.setup_util import get_sim_env_class
+
+
+@configclass
+class Args:
+    """Arguments for the static scene."""
+
+    robot: str = "franka"
+
+    ## Handlers
+    sim: Literal["isaaclab", "isaacgym", "genesis", "pyrep", "pybullet", "sapien", "sapien3", "mujoco", "blender"] = (
+        "isaaclab"
+    )
+
+    ## Others
+    num_envs: int = 1
+    headless: bool = False
+
+    def __post_init__(self):
+        """Post-initialization configuration."""
+        log.info(f"Args: {self}")
+
+
+args = tyro.cli(Args)
+
+# initialize scenario
+scenario = ScenarioCfg(
+    robot=args.robot,
+    try_add_table=False,
+    sim=args.sim,
+    headless=args.headless,
+    num_envs=args.num_envs,
+)
+
+# add cameras
+scenario.cameras = [
+    PinholeCameraCfg("cam0", width=1024, height=1024, pos=(1.5, -1.5, 1.5), look_at=(0.0, 0.0, 0.0)),
+    PinholeCameraCfg("cam1", width=1024, height=1024, pos=(1.5, 1.5, 1.5), look_at=(0.0, 0.0, 0.0)),
+]
+
+# add objects
+scenario.objects = [
+    PrimitiveCubeCfg(
+        name="cube",
+        size=(0.1, 0.1, 0.1),
+        color=[1.0, 0.0, 0.0],
+        physics=PhysicStateType.RIGIDBODY,
+    ),
+    PrimitiveSphereCfg(
+        name="sphere",
+        radius=0.1,
+        color=[0.0, 0.0, 1.0],
+        physics=PhysicStateType.RIGIDBODY,
+    ),
+    RigidObjCfg(
+        name="bbq_sauce",
+        scale=(2, 2, 2),
+        physics=PhysicStateType.RIGIDBODY,
+        usd_path="get_started/example_assets/bbq_sauce/usd/bbq_sauce.usd",
+        urdf_path="get_started/example_assets/bbq_sauce/urdf/bbq_sauce.urdf",
+        mjcf_path="get_started/example_assets/bbq_sauce/mjcf/bbq_sauce.xml",
+    ),
+    ArticulationObjCfg(
+        name="box_base",
+        fix_base_link=True,
+        usd_path="get_started/example_assets/box_base/usd/box_base.usd",
+        urdf_path="get_started/example_assets/box_base/urdf/box_base_unique.urdf",
+        mjcf_path="get_started/example_assets/box_base/mjcf/box_base_unique.mjcf",
+    ),
+]
+
+
+log.info(f"Using simulator: {args.sim}")
+env_class = get_sim_env_class(SimType(args.sim))
+env = env_class(scenario)
+
+init_states = [
+    {
+        "objects": {
+            "cube": {
+                "pos": torch.tensor([0.3, -0.2, 0.05]),
+                "rot": torch.tensor([1.0, 0.0, 0.0, 0.0]),
+            },
+            "sphere": {
+                "pos": torch.tensor([0.4, -0.6, 0.05]),
+                "rot": torch.tensor([1.0, 0.0, 0.0, 0.0]),
+            },
+            "bbq_sauce": {
+                "pos": torch.tensor([0.7, -0.3, 0.14]),
+                "rot": torch.tensor([1.0, 0.0, 0.0, 0.0]),
+            },
+            "box_base": {
+                "pos": torch.tensor([0.5, 0.2, 0.1]),
+                "rot": torch.tensor([0.0, 0.7071, 0.0, 0.7071]),
+                "dof_pos": {"box_joint": 0.0},
+            },
+        },
+        "robots": {
+            "franka": {
+                "pos": torch.tensor([0.0, 0.0, 0.0]),
+                "rot": torch.tensor([1.0, 0.0, 0.0, 0.0]),
+                "dof_pos": {
+                    "panda_joint1": 0.0,
+                    "panda_joint2": -0.785398,
+                    "panda_joint3": 0.0,
+                    "panda_joint4": -2.356194,
+                    "panda_joint5": 0.0,
+                    "panda_joint6": 1.570796,
+                    "panda_joint7": 0.785398,
+                    "panda_finger_joint1": 0.04,
+                    "panda_finger_joint2": 0.04,
+                },
+            },
+        },
+    }
+]
+obs, extras = env.reset(states=init_states)
+os.makedirs("get_started/output", exist_ok=True)
+save_path = f"get_started/output/multiple_cameras_{args.sim}.png"
+log.info(f"Saving image to {save_path}")
+
+img_cat = np.concatenate([obs.cameras["cam0"].rgb[0].cpu().numpy(), obs.cameras["cam1"].rgb[0].cpu().numpy()], axis=1)
+imageio.imwrite(save_path, img_cat)
