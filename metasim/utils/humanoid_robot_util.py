[fix] align quaternion in humanoid infra (#286)

metasim/utils/humanoid_robot_util.py
@@ -2,16 +2,15 @@
 
 from __future__ import annotations
 
-import numpy as np
 import torch
 
-try:
-    from isaacgym.torch_utils import get_euler_xyz
-except ImportError:
-    pass
-
-
-from metasim.utils.math import axis_angle_from_quat, matrix_from_quat, quat_from_angle_axis, quat_mul
+from metasim.utils.math import (
+    axis_angle_from_quat,
+    euler_xyz_from_quat,
+    matrix_from_quat,
+    quat_from_angle_axis,
+    quat_mul,
+)
 
 
 def torso_upright(envstate, robot_name: str):
@@ -219,10 +218,10 @@ def get_euler_xyz_tensor(quat):
     Returns:
         torch.Tensor: Euler angles tensor of shape (N, 3) where each row contains (roll, pitch, yaw).
     """
-    r, p, w = get_euler_xyz(quat)
+    r, p, w = euler_xyz_from_quat(quat)
     # stack r, p, w in dim1
     euler_xyz = torch.stack((r, p, w), dim=1)
-    euler_xyz[euler_xyz > np.pi] -= 2 * np.pi
+    euler_xyz[euler_xyz > torch.pi] -= 2 * torch.pi
     return euler_xyz
 
 

roboverse_learn/skillblender_rl/env_wrappers/base/humanoid_base_wrapper.py
@@ -103,9 +103,7 @@ class HumanoidBaseWrapper(RslRlWrapper):
 
         TODO: move those var into
         """
-        self.base_quat = (
-            torch.tensor([0.0, 0.0, 0.0, 1.0], device=self.device).unsqueeze(0).repeat(self.num_envs, 1)
-        )  # TODO align it with metasim quaternion format [wxyz]
+        self.base_quat = torch.tensor([1.0, 0.0, 0.0, 0.0], device=self.device).unsqueeze(0).repeat(self.num_envs, 1)
         self.base_euler_xyz = get_euler_xyz_tensor(self.base_quat)
 
         self.obs_buf = torch.zeros(self.num_envs, self.num_obs, device=self.device, dtype=torch.float)
@@ -509,7 +507,7 @@ class HumanoidBaseWrapper(RslRlWrapper):
         self.episode_length_buf[env_ids] = 0
         self.feet_air_time[env_ids] = 0.0
         self.base_quat[env_ids] = (
-            torch.tensor([0.0, 0.0, 0.0, 1.0], device=self.device, dtype=torch.float32)
+            torch.tensor([1.0, 0.0, 0.0, 0.0], device=self.device, dtype=torch.float32)
             .unsqueeze(0)
             .repeat(len(env_ids), 1)
         )